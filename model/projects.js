b9c51f04827f9c0cd551a2a184cd16c5127ca691
darrenrtee <45459431+darrenrtee@users.noreply.github.com>
2019-10-24 02:00:51 +0800

    Updated Orders

--- model/projects.js
+++ model/projects.js
@@ -1,12 +1,16 @@
 const Database = require("./database");
 var database = new Database();
 
-exports.create = function(projectName, clientID) {
+exports.create = function(projectName, clientID,projectNumber,dateAdded) {
     Promise.resolve(database.query("SELECT COUNT(projectID) AS 'count' FROM projects")).then(function(value) {
-        database.query("INSERT INTO projects (projectID, projectName, clientID) VALUES ?", [[[(value[0].count + 1), projectName, clientID]]])
+        database.query("INSERT INTO projects (projectID, projectName, clientID,projectNumber,dateAdded) VALUES ?", [[[(value[0].count + 1), projectName, clientID,projectNumber,dateAdded]]])
     })
 }
 
 exports.getAll = function() {
     return database.query("SELECT * FROM projects");
+}
+
+exports.getLatest = function(){
+    return database.query("SELECT * FROM projects ORDER BY projects.projectNumber DESC LIMIT 1")
 }
