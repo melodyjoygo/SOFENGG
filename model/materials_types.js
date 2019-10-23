e997c4abdec29df871322f8acbc183e593f8e767
darrenrtee <45459431+darrenrtee@users.noreply.github.com>
2019-10-23 19:20:49 +0800

    Update

--- model/materials_types.js
+++ model/materials_types.js
@@ -1,6 +1,13 @@
 const Database = require("./database");
 var database = new Database();
 
+exports.create = function(materialType) {
+    Promise.resolve(database.query("SELECT COUNT(mtID) AS 'count' FROM material_types")).then(function(value) {
+        database.query("INSERT INTO material_types (mtID, type) VALUES ?", [[[(value[0].count + 1), materialType]]])
+      
+    })
+}
+
 exports.getType = function(mtID){
     return database.query("SELECT * FROM material_types WHERE material_types.mtID = ?",[mtID]);
 }
