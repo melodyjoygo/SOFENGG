025ee609e3a5f31d9f46e324575cb8c075cc62a1
Libelous <delta1442@gmail.com>
2019-10-24 17:11:20 +0800

    Update DB SQL LATEST(PLS UPDATE QA).sql

--- DB SQL LATEST(PLS UPDATE QA).sql
+++ DB SQL LATEST(PLS UPDATE QA).sql
@@ -344,7 +344,7 @@ CREATE TABLE `year_tracker` (
   `yearID` int(11) NOT NULL,
   `currYear` int(11) NOT NULL,
   PRIMARY KEY (`yearID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
