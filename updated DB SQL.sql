bdc24c9f0f1645f197d105a3f2f25392f71221ee
Melody <melody_go@dlsu.edu.ph>
2019-11-05 23:33:08 +0800

    Update updated DB SQL.sql

--- updated DB SQL.sql
+++ updated DB SQL.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `softengdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `softengdb`;
 -- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: softengdb
@@ -33,10 +35,12 @@ CREATE TABLE `clients` (
 -- Dumping data for table `clients`
 --
 
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
 INSERT INTO `clients` VALUES (1,'Riot Games');
 INSERT INTO `clients` VALUES (2,'Samsung');
 INSERT INTO `clients` VALUES (3,'Adidas');
 INSERT INTO `clients` VALUES (4,'Apple');
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
 
 --
 -- Table structure for table `inventory`
@@ -59,8 +63,10 @@ CREATE TABLE `inventory` (
 -- Dumping data for table `inventory`
 --
 
+/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
 INSERT INTO `inventory` VALUES (1,1,0,'2019-10-22');
 INSERT INTO `inventory` VALUES (2,2,0,'2019-11-04');
+/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
 
 --
 -- Table structure for table `material_types`
@@ -80,12 +86,14 @@ CREATE TABLE `material_types` (
 -- Dumping data for table `material_types`
 --
 
+/*!40000 ALTER TABLE `material_types` DISABLE KEYS */;
 INSERT INTO `material_types` VALUES (1,'Plastic');
 INSERT INTO `material_types` VALUES (2,'Wood');
 INSERT INTO `material_types` VALUES (3,'Acrylic');
 INSERT INTO `material_types` VALUES (4,'Metal');
 INSERT INTO `material_types` VALUES (5,'Bronze');
 INSERT INTO `material_types` VALUES (6,'Gold');
+/*!40000 ALTER TABLE `material_types` ENABLE KEYS */;
 
 --
 -- Table structure for table `materials`
@@ -110,8 +118,10 @@ CREATE TABLE `materials` (
 -- Dumping data for table `materials`
 --
 
+/*!40000 ALTER TABLE `materials` DISABLE KEYS */;
 INSERT INTO `materials` VALUES (1,'2x4',2,1,100,1);
 INSERT INTO `materials` VALUES (2,'2x4',1,1,100,1);
+/*!40000 ALTER TABLE `materials` ENABLE KEYS */;
 
 --
 -- Table structure for table `project_materials`
@@ -137,6 +147,8 @@ CREATE TABLE `project_materials` (
 -- Dumping data for table `project_materials`
 --
 
+/*!40000 ALTER TABLE `project_materials` DISABLE KEYS */;
+/*!40000 ALTER TABLE `project_materials` ENABLE KEYS */;
 
 --
 -- Table structure for table `projects`
@@ -150,6 +162,7 @@ CREATE TABLE `projects` (
   `clientID` int(11) DEFAULT NULL,
   `projectNumber` varchar(45) DEFAULT NULL,
   `dateAdded` date DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`projectID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -158,10 +171,12 @@ CREATE TABLE `projects` (
 -- Dumping data for table `projects`
 --
 
-INSERT INTO `projects` VALUES (1,1,'P19010-00000','2019-10-24');
-INSERT INTO `projects` VALUES (2,2,'P19010-00001','2019-10-24');
-INSERT INTO `projects` VALUES (3,3,'P19010-00002','2019-10-24');
-INSERT INTO `projects` VALUES (4,4,'P1911-00003','2019-11-04');
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+INSERT INTO `projects` VALUES (1,1,'P19010-00000','2019-10-24',NULL);
+INSERT INTO `projects` VALUES (2,2,'P19010-00001','2019-10-24',NULL);
+INSERT INTO `projects` VALUES (3,3,'P19010-00002','2019-10-24',NULL);
+INSERT INTO `projects` VALUES (4,4,'P1911-00003','2019-11-04',NULL);
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 
 --
 -- Table structure for table `request_types`
@@ -181,6 +196,8 @@ CREATE TABLE `request_types` (
 -- Dumping data for table `request_types`
 --
 
+/*!40000 ALTER TABLE `request_types` DISABLE KEYS */;
+/*!40000 ALTER TABLE `request_types` ENABLE KEYS */;
 
 --
 -- Table structure for table `requests`
@@ -208,6 +225,8 @@ CREATE TABLE `requests` (
 -- Dumping data for table `requests`
 --
 
+/*!40000 ALTER TABLE `requests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `requests` ENABLE KEYS */;
 
 --
 -- Table structure for table `suppliers`
@@ -227,9 +246,11 @@ CREATE TABLE `suppliers` (
 -- Dumping data for table `suppliers`
 --
 
+/*!40000 ALTER TABLE `suppliers` DISABLE KEYS */;
 INSERT INTO `suppliers` VALUES (1,'Samsung');
 INSERT INTO `suppliers` VALUES (2,'Addidas');
 INSERT INTO `suppliers` VALUES (3,'Sadasdas');
+/*!40000 ALTER TABLE `suppliers` ENABLE KEYS */;
 
 --
 -- Table structure for table `transactions`
@@ -253,8 +274,10 @@ CREATE TABLE `transactions` (
 -- Dumping data for table `transactions`
 --
 
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
 INSERT INTO `transactions` VALUES (1,1,1,100,'2019-10-23','Pending',2);
 INSERT INTO `transactions` VALUES (2,1,1,1222,'2019-11-04','Pending',12);
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
 
 --
 -- Table structure for table `unit_of_measures`
@@ -274,9 +297,11 @@ CREATE TABLE `unit_of_measures` (
 -- Dumping data for table `unit_of_measures`
 --
 
+/*!40000 ALTER TABLE `unit_of_measures` DISABLE KEYS */;
 INSERT INTO `unit_of_measures` VALUES (1,'pc');
 INSERT INTO `unit_of_measures` VALUES (2,'block');
 INSERT INTO `unit_of_measures` VALUES (3,'pint');
+/*!40000 ALTER TABLE `unit_of_measures` ENABLE KEYS */;
 
 --
 -- Table structure for table `user_types`
@@ -296,11 +321,13 @@ CREATE TABLE `user_types` (
 -- Dumping data for table `user_types`
 --
 
+/*!40000 ALTER TABLE `user_types` DISABLE KEYS */;
 INSERT INTO `user_types` VALUES (0,'Super Admin');
 INSERT INTO `user_types` VALUES (1,'User Admin');
 INSERT INTO `user_types` VALUES (2,'Admin');
 INSERT INTO `user_types` VALUES (3,'Clerk');
 INSERT INTO `user_types` VALUES (4,'Stockman');
+/*!40000 ALTER TABLE `user_types` ENABLE KEYS */;
 
 --
 -- Table structure for table `users`
@@ -324,7 +351,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES (1,'Darren Tee','dardartee7@gmail.com','U2FsdGVkX19Bb2wGzgv+J2R3r0BOMnR94+fH/S0nxDY=',1);
+INSERT INTO `users` VALUES (2,'melody go','melody@gmail.com','U2FsdGVkX1/EwCB8Ryml4lUxcOmWvwFH6lGa2bpOavA=',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 --
 -- Table structure for table `year_tracker`
@@ -344,7 +374,9 @@ CREATE TABLE `year_tracker` (
 -- Dumping data for table `year_tracker`
 --
 
+/*!40000 ALTER TABLE `year_tracker` DISABLE KEYS */;
 INSERT INTO `year_tracker` VALUES (1,2019);
+/*!40000 ALTER TABLE `year_tracker` ENABLE KEYS */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -355,4 +387,4 @@ INSERT INTO `year_tracker` VALUES (1,2019);
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-04 21:43:45
+-- Dump completed on 2019-11-05 23:32:40
