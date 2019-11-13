4a53a79e8b9e5fc408fecc089952756dd48b59dc
JitsLumba <44419666+JitsLumba@users.noreply.github.com>
2019-11-14 00:38:22 +0800

    Changed root password

--- model/database.js
+++ model/database.js
@@ -6,7 +6,7 @@ class Database {
             host: 'localhost',
             user: 'root', 
             port: '3306',
-            password: 'lodi',
+            password: 'root',
             database: 'softengdb'
         });
     }
