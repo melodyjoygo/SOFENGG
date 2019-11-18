a223fd8ba95ab4287cca83365b3b80590b1d8639
Melody <melody_go@dlsu.edu.ph>
2019-11-18 19:34:37 +0800

    fixed x in add item modal

--- model/database.js
+++ model/database.js
@@ -6,7 +6,7 @@ class Database {
             host: 'localhost',
             user: 'root', 
             port: '3306',
-            password: 'root',
+            password: 'lodi',
             database: 'softengdb'
         });
     }
