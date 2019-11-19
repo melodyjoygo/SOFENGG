3c6519fbedf04388ad110166b13985fdf9c529af
darrenrtee <45459431+darrenrtee@users.noreply.github.com>
2019-11-19 10:44:53 +0800

    Updates

--- model/transaction.js
+++ model/transaction.js
@@ -1,9 +1,9 @@
 const Database = require("./database");
 var database = new Database();
 
-exports.create = function(supplierID,materialID ,price, date, status,quantity) {
+exports.create = function(supplierID,materialID ,price, date, status,quantity,poNumber) {
     Promise.resolve(database.query("SELECT COUNT(transactionID) AS 'count' FROM transactions")).then(function(value) {
-        database.query("INSERT INTO transactions (transactionID,materialID, supplierID, price, date, status,quantity) VALUES ?", [[[(value[0].count + 1),materialID,supplierID, price, date, status,quantity]]])
+        database.query("INSERT INTO transactions (transactionID,materialID, supplierID, price, date, status,quantity,poNumber) VALUES ?", [[[(value[0].count + 1),materialID,supplierID, price, date, status,quantity,poNumber]]])
     })
 }
 
@@ -16,6 +16,6 @@ exports.loadItems = function(){
     return database.query("SELECT * FROM transactions NATURAL JOIN suppliers NATURAL JOIN materials LEFT JOIN material_types ON materials.materialType = material_types.mtID")
 }
 
-exports.edit = function(materialID,supplierID,price,quantity,transactionID){
-    database.query("UPDATE transactions SET materialID = ?, supplierID = ?, price = ? ,quantity = ? WHERE transactionID = ?",[materialID,supplierID,price,quantity,transactionID])
+exports.edit = function(materialID,supplierID,price,quantity,transactionID,poNumber){
+    database.query("UPDATE transactions SET materialID = ?, supplierID = ?, price = ? ,quantity = ? ,poNumber = ? WHERE transactionID = ?",[materialID,supplierID,price,quantity,poNumber,transactionID])
 }
