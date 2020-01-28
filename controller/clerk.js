f5f1485ecf5d71c23d9e0effe289339000dd1092
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2020-01-28 09:13:40 +0800

    Revert "Revert "Bug Fix""
    This reverts commit 64f646166173b5fc300aeeff2ea5c639283d6d80.

--- controller/clerk.js
+++ controller/clerk.js
@@ -179,4 +179,30 @@ router.post("/editPrice",(req,res)=>{
     }
 })
 
+router.post("/orders/add",(req,res)=>{
+    let qty = req.body.qty
+    let cost = req.body.unitcost
+    let itemID = req.body.item
+    let poNumber = req.body.poNumber
+    var empty = false
+    
+    if(qty === "" || cost === "" || itemID === "" || poNumber === "")
+        empty = true
+    
+    if(empty){
+        res.render("order.hbs",{
+            message:1
+        })
+    }
+    else{
+        Promise.resolve(Materials.findItem(itemID)).then(function(value){
+            Promise.resolve(Items.create(value[0].supplierID,value[0].materialID,cost,new Date().toISOString().slice(0, 19).replace('T', ' '),'Pending',qty,poNumber)).then(function(value){
+                res.render("order.hbs",{
+                    message:2
+                })  
+            })
+        })
+    }
+})
+
 module.exports = router;
